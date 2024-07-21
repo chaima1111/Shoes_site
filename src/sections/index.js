1c747ed8aebdde5049a4844a2ddde6ebf04aabee
chaima1111 <chaimacheniti19@gmail.com>
2024-07-21 18:50:49 +0100

    adding essential components  of the project

---
+++ src/sections/index.js
@@ -0,0 +1,22 @@
+import Hero from './Hero';
+import Nav from "./Nav";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+
+
+export {
+  Hero,
+  Nav,
+  Subscribe,
+  Footer,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews
+};
